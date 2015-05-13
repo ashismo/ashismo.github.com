6223925cc71c4a102aba41d9a05a44670df14962
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-13 19:02:11 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -2,20 +2,19 @@ $(document).ready(function() {
    // Header in all tables will be center aligned 
    $("thead tr", this).children("*").attr("style", "text-align: center");
    
+   // Copy text into clipboard
    $("pre").hover(function() {
        $( this ).prepend( $( '<div style="text-align:right;"><button class="copy-button github"></button></div>' ) );
-       $(".copy-button").click(function() {
-         alert($(".copy-button").parent().html());
-      });
+         $(".copy-button").click(function() {
+            alert($(".copy-button").parent().parent().html().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
+         });
+         $(".copy-button").hover(function() {
+            $( this ).parent().apend( $( '<div style="text-align:right;">Copy into clipboard</button></div>' ) );
+           }, function() {
+             $( this ).parent().find( "div:last" ).remove();
+         });
      }, function() {
        $( this ).find( "button:first" ).remove();
    });
-   
-   /*$(".copy-button").hover(function() {
-      $( this ).parent().apend( $( '<div style="text-align:right;">Copy into clipboard</button></div>' ) );
-     }, function() {
-       $( this ).parent().find( "div:last" ).remove();
-   });*/
-   
-   
+
 });
