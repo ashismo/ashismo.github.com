994a8d095ad4fc2e93230ebe609716738fe166f8
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-13 19:11:05 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
    
    // Copy text into clipboard
    $("pre").hover(function() {
-       $( this ).prepend( $( '<div style="text-align:right;"><button class="copy-button github"></button></div>' ) );
+       $( this ).prepend( $( '<div style="text-align:right;"><button class="copy-button github"></button>Copy into Clipboard</div>' ) );
          $(".copy-button").click(function() {
             alert($(".copy-button").parent().parent().html().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
          });
          $(".copy-button").hover(function() {
-            $(".copy-button").parent().apend( $( '<div style="text-align:right;">Copy into clipboard</button></div>' ) );
+            //$(".copy-button").parent().apend( $( '<div style="text-align:right;">Copy into clipboard</button></div>' ) );
+            alert($(".copy-button").parent().parent().html().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
            }, function() {
              $(".copy-button").parent().find( "div:last" ).remove();
          });
