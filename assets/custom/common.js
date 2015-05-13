51aac1360ea04242d30d9073ff3b25a90f2a8cb7
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-13 16:18:17 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
    // Header in all tables will be center aligned 
    $("thead tr", this).children("*").attr("style", "text-align: center");
+   
+   $("pre").hover(function() {
+       $( this ).append( $( "<span> ***</span>" ) );
+     }, function() {
+       $( this ).find( "span:last" ).remove();
+   });
 });
