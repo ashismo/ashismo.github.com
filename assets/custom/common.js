bb54ac4108e73deacc9e73db08afbff1e14a343f
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-14 13:52:54 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -10,9 +10,9 @@ $(document).ready(function() {
        //$( this ).prepend( $( '<div style="text-align:right;" id="copyIntoClipBoard"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>' ) );
          $( this ).find("div").css("visibility", "visible");
          $( this ).find("div").find("button").addClass("selectedButton");
-         $( this ).find(".copy-button").click(function() {
+         $(".selectedButton").click(function() {
             //alert($( this ).find("pre").html().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
-            alert($( this ).parent().parent().parent().find("pre").html());
+            alert($(".selectedButton").parent().parent().parent().find("pre").html());
          });
         /* $(".copy-button").hover(function() {
             //$(".copy-button").parent().apend( $( '<div style="text-align:right;">Copy into clipboard</button></div>' ) );
