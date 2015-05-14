71010cc039d5f73f7f9a63b17aa27e70be0b3b76
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-14 13:19:36 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
    // Header in all tables will be center aligned 
    $("thead tr", this).children("*").attr("style", "text-align: center");
    
+   // Add copy to clipboard button before every pre
+    $("pre").wrap("<div class='codeSnippet'></div>");
+    $(".codeSnippet").prepend('<div style="text-align:right; display:none;" id="copyIntoClipBoard"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>');
    // Copy text into clipboard
    $("pre").hover(function() {
-       $( this ).prepend( $( '<div style="text-align:right;" id="copyIntoClipBoard"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>' ) );
+       //$( this ).prepend( $( '<div style="text-align:right;" id="copyIntoClipBoard"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>' ) );
+         $( this ).parent().css("display", "block");
          $(".copy-button").click(function() {
             alert($("#copyIntoClipBoard").parent().html().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
          });
@@ -15,7 +19,8 @@ $(document).ready(function() {
              $(".copy-button").parent().find( "div:last" ).remove();
          });
      }, function() {
-       $( "#copyIntoClipBoard" ).remove();
+       //$( "#copyIntoClipBoard" ).remove();
+       $( this ).parent().css("display", "none");
    });
 
 });
