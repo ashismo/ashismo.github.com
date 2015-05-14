8fa5081b1ec116bf41646f10315f5fde7983d480
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-14 17:08:45 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -4,28 +4,29 @@ $(document).ready(function() {
       $("thead tr", this).children("*").attr("style", "text-align: center");
    }
    // Add copy to clipboard button before every pre
-    $("pre").wrap("<div class='codeSnippet'></div>");
-    $(".codeSnippet").prepend('<div style="text-align:right; visibility:hidden;"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>');
-   // Copy text into clipboard
-   $(".codeSnippet").hover(function() {
-         $( this ).find("div:first").css("visibility", "visible");
-         $( this ).prepend('<div style="display:none;" id="codeCopied"></div>');
-         $( this ).find("div").find("button").addClass("selectedButton");
-         $( this ).find("div").find("button").attr("id", "clickedButton")
-     }, function() {
-       $( "#codeCopied" ).remove();
-       $( this ).find("div").find("button").removeClass("selectedButton");
-       $( this ).find("div").find("button").removeAttr("id");
-       $( this ).find("div:first").css("visibility", "hidden");
-   });
-   
-   $(".copy-button").click(function() {
-         // If the button is visible
-         if($( this ).parent().parent().is(':visible')) {
-            //alert($(".selectedButton").parent().parent().parent().find("pre").html());
-            alert($("#codeCopied").html());
-         }
+   if($("pre") !== null) {
+       $("pre").wrap("<div class='codeSnippet'></div>");
+       $(".codeSnippet").prepend('<div style="text-align:right; visibility:hidden;"><div><button class="copy-button github"></button></div><div style="color:red">Copy into Clipboard</div></div>');
+      // Copy text into clipboard
+      $(".codeSnippet").hover(function() {
+            $( this ).find("div:first").css("visibility", "visible");
+            $( this ).prepend('<div style="display:none;" id="codeCopied"></div>');
+            $( this ).find("div").find("button").addClass("selectedButton");
+            $( this ).find("div").find("button").attr("id", "clickedButton")
+        }, function() {
+          $( "#codeCopied" ).remove();
+          $( this ).find("div").find("button").removeClass("selectedButton");
+          $( this ).find("div").find("button").removeAttr("id");
+          $( this ).find("div:first").css("visibility", "hidden");
       });
-
+      
+      $(".copy-button").click(function() {
+            // If the button is visible
+            if($( this ).parent().parent().is(':visible')) {
+               //alert($(".selectedButton").parent().parent().parent().find("pre").html());
+               alert($("#codeCopied").html());
+            }
+         });
+   }
 });
 
