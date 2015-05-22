1b68c0910ae9478ce5fa36332e41541127b9493c
Ashish Mondal <ashishkrmondal@gmail.com>
2015-05-22 18:18:43 +0530

    Update common.js

--- assets/custom/common.js
+++ assets/custom/common.js
@@ -99,8 +99,8 @@ $(document).ready(function() {
                     clientText.setText( args.text );
                     //$('#text-to-copy-text').fadeIn();
                 } );
-            	} );
-            });
+       	    } );
+
 		
 		
 		$(this).closest('.codeSnippet').find('pre').removeSelection();
